d838604ee06a32978e367775b1b03180faf6a39f
Taylor Clawson <taylor.clawson@gmail.com>
2017-07-07 21:36:44 -0400

    Fixing some of the changes I made before...that were lost in merging somehow.

--- gather_bee.py
+++ gather_bee.py
@@ -1,78 +1,11 @@
 import sys
 sys.path.append('.')
+sys.path.append(r'C:\Users\taylo\OneDrive\Cornell\LISC\Code\RoboBee\PyBee3D_Basic')
 import pytry
 import numpy as np
-import robobee
-import scipy
 import nengo
-from controllers.pif.pif_compensator import PIFCompensator
-import seaborn
-
-class PIFControl(nengo.Network):
-    def __init__(self, bee, label=None):
-        self.bee = bee
-        super(PIFControl, self).__init__(label=label)
-        self.controller = PIFCompensator()
-
-        def control_ode_fun(t, eta, y_star, x, u, bee):
-            return self.controller.get_control_dynamics(eta, t, y_star, x, u, bee)
-        self.integrator_control = scipy.integrate.ode(control_ode_fun).set_integrator('dopri5')
-        eta = np.zeros(7)
-        self.integrator_control.set_initial_value(eta, 0)
-        
-        with self:
-            self.control = nengo.Node(self.update, size_in=28)
-            
-            self.y_star = nengo.Node(None, size_in=4)
-            self.x = nengo.Node(None, size_in=20)
-            self.u = nengo.Node(None, size_in=4)
-            nengo.Connection(self.y_star, self.control[:4], synapse=None)
-            nengo.Connection(self.x, self.control[4:24], synapse=None)
-            nengo.Connection(self.u, self.control[24:], synapse=None)
-            
-    def update(self, t, v):
-        y_star, x, u = v[:4], v[4:24], v[24:28]
-        self.integrator_control.set_f_params(y_star, x, u, self.bee)
-        eta = self.integrator_control.integrate(t)
-        u = eta[:4]
-        return u
-        
-
-class NengoBee(nengo.Network):
-    def __init__(self, label=None, pose_offset=(0,0,0), dpose_offset=(0,0,0)):
-        super(NengoBee, self).__init__(label=label)
-
-        self.bee = robobee.RoboBee()
-
-        traj_data = scipy.io.loadmat('Hover_Data.mat')
-        x = traj_data['x'][0]
-
-        x[8:11] += pose_offset
-        x[14:17] += dpose_offset
-
-        self.integrator_dynamics = scipy.integrate.ode(self.bee.get_dynamics).set_integrator('dopri5')
-        self.integrator_dynamics.set_initial_value(x, 0)
-
-        self.u_0 = traj_data['u'][0]
-
-        with self:
-            
-            self.plant = nengo.Node(self.update, size_in=len(self.u_0))
-            
-            self.u = nengo.Node(None, size_in=self.plant.size_in)
-            nengo.Connection(self.u, self.plant, synapse=None)
-
-            self.xyz = nengo.Node(None, size_in=3)
-            nengo.Connection(self.plant[11:14], self.xyz, synapse=None)
-
-            self.attitude = nengo.Node(None, size_in=3)
-            nengo.Connection(self.plant[8:11], self.attitude, synapse=None)
-
-
-    def update(self, t, u):
-        self.integrator_dynamics.set_f_params(u)
-        x = self.integrator_dynamics.integrate(t)
-        return x
+from pif_control import PIFControl
+from nengo_bee import NengoBee
 
 class GatherDataTrial(pytry.PlotTrial):
     def params(self):
@@ -87,8 +20,6 @@ class GatherDataTrial(pytry.PlotTrial):
 
         model = nengo.Network()
         with model:
-
-
             pose_offset = np.random.uniform(-p.pose_var, p.pose_var, size=3)
             dpose_offset = np.random.uniform(-p.dpose_var, p.dpose_var, size=3)
 
@@ -138,9 +69,8 @@ class GatherDataTrial(pytry.PlotTrial):
             plt.ylabel('u')
             plt.legend(['stroke ampl.', 'pitch torque', 'yaw torque', 'roll'], loc='best')
 
-
         return dict(
-            x=sim.data[probe_x],
+            x_world=sim.data[probe_x],
             u=sim.data[probe_u])
 
         
--- neuron_bee.py
+++ neuron_bee.py
@@ -1,93 +1,15 @@
 import sys
-sys.path.append('.')
+# sys.path.append('.')
+sys.path.append(r'C:\Users\taylo\OneDrive\Cornell\LISC\Code\RoboBee\PyBee3D_Basic')
 import pytry
 import numpy as np
 import robobee
 import scipy
 import nengo
-from controllers.pif.pif_compensator import PIFCompensator
+# from controllers.pif.pif_compensator import PIFCompensator
+from pif_control import PIFControl
+from nengo_bee import NengoBee
 import seaborn
-
-class PIFControl(nengo.Network):
-    def __init__(self, bee, label=None):
-        self.bee = bee
-        super(PIFControl, self).__init__(label=label)
-        self.controller = PIFCompensator()
-
-        def control_ode_fun(t, eta, y_star, x, u, bee):
-            return self.controller.get_control_dynamics(eta, t, y_star, x, u, bee)
-        self.integrator_control = scipy.integrate.ode(control_ode_fun).set_integrator('dopri5')
-        eta = np.zeros(7)
-        eta[:4] = self.controller.get_u_star([0,0,0,0])
-        self.integrator_control.set_initial_value(eta, 0)
-        
-        with self:
-            self.control = nengo.Node(self.update, size_in=28)
-            
-            self.y_star = nengo.Node(None, size_in=4)
-            self.x = nengo.Node(None, size_in=20)
-            self.u = nengo.Node(None, size_in=4)
-            nengo.Connection(self.y_star, self.control[:4], synapse=None)
-            nengo.Connection(self.x, self.control[4:24], synapse=None)
-            nengo.Connection(self.u, self.control[24:], synapse=None)
-            
-    def update(self, t, v):
-        y_star, x, u = v[:4], v[4:24], v[24:28]
-        self.integrator_control.set_f_params(y_star, x, u, self.bee)
-        eta = self.integrator_control.integrate(t)
-        u = eta[:4]
-        return u
-        
-
-class NengoBee(nengo.Network):
-    def __init__(self, label=None, pose_offset=(0,0,0), dpose_offset=(0,0,0)):
-        super(NengoBee, self).__init__(label=label)
-
-        self.bee = robobee.RoboBee()
-
-        traj_data = scipy.io.loadmat('Hover_Data.mat')
-        x = traj_data['x'][0]
-
-        x[8:11] += pose_offset
-        x[14:17] += dpose_offset
-
-        self.integrator_dynamics = scipy.integrate.ode(self.bee.get_dynamics).set_integrator('dopri5')
-        self.integrator_dynamics.set_initial_value(x, 0)
-
-        self.u_0 = traj_data['u'][0]
-
-        with self:
-            
-            self.plant = nengo.Node(self.update, size_in=len(self.u_0))
-            
-            self.u = nengo.Node(None, size_in=self.plant.size_in)
-            nengo.Connection(self.u, self.plant, synapse=None)
-
-            self.xyz = nengo.Node(None, size_in=3)
-            nengo.Connection(self.plant[11:14], self.xyz, synapse=None)
-
-            self.attitude = nengo.Node(None, size_in=3)
-            nengo.Connection(self.plant[8:11], self.attitude, synapse=None)
-
-            self.xyz_rate = nengo.Node(None, size_in=3)
-            nengo.Connection(self.plant[17:20], self.xyz_rate, synapse=None)
-
-            self.attitude_rate = nengo.Node(None, size_in=3)
-            nengo.Connection(self.plant[14:17], self.attitude_rate, synapse=None)
-
-
-            def rotate(t, x):
-                body_x = self.bee.world_state_to_body(x)
-                return body_x[17:20]
-
-            self.xyz_rate_body = nengo.Node(rotate, size_in=20)
-            nengo.Connection(self.plant, self.xyz_rate_body, synapse=None)
-
-
-    def update(self, t, u):
-        self.integrator_dynamics.set_f_params(u)
-        x = self.integrator_dynamics.integrate(t)
-        return x
 
 class GatherDataTrial(pytry.NengoTrial):
     def params(self):
